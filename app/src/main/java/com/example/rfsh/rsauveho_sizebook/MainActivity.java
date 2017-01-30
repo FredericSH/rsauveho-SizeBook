af58e8a1a06812d20392b7f7a41c73c4f699d72b
Frederic Sauve-Hoover <rsauveho@ualberta.ca>
2017-01-29 19:01:54 -0700

    adding file IO

--- app/src/main/java/com/example/rfsh/rsauveho_sizebook/MainActivity.java
+++ app/src/main/java/com/example/rfsh/rsauveho_sizebook/MainActivity.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,14 +27,18 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Record> recordList;
     private ArrayAdapter<Record> adapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        storedRecordList = (ListView) findViewById(R.id.recordList);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,5 +46,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        loadRecords();
+
+        adapter = new ArrayAdapter<Record>(this, R.layout.list_item, recordList);
+        storedRecordList.setAdapter(adapter);
+    }
+
+    private void loadRecords() {
+        try {
+            FileInputStream fileInputStream = openFileInput(FILENAME);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+
+            Gson gson = new Gson();
+
+            Type listType = new TypeToken<ArrayList<Record>>(){}.getType();
+            recordList = gson.fromJson(bufferedReader, listType);
+        }
+        catch (FileNotFoundException e) {
+            recordList = new ArrayList<Record>();
+        }
     }
 }
--- app/src/main/java/com/example/rfsh/rsauveho_sizebook/RecordActivity.java
+++ app/src/main/java/com/example/rfsh/rsauveho_sizebook/RecordActivity.java
@@ -1,13 +1,74 @@
 package com.example.rfsh.rsauveho_sizebook;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class RecordActivity extends AppCompatActivity {
+
+    private static final String FILENAME = "size_book_saves.sav";
+
+    private ArrayList<Record> recordList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        loadRecords();
+    }
+
+    private void loadRecords() {
+        try {
+            FileInputStream fileInputStream = openFileInput(FILENAME);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+
+            Gson gson = new Gson();
+
+            Type listType = new TypeToken<ArrayList<Record>>(){}.getType();
+            recordList = gson.fromJson(bufferedReader, listType);
+        }
+        catch (FileNotFoundException e) {
+            recordList = new ArrayList<Record>();
+        }
+    }
+
+    private void saveRecords() {
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+
+            Gson gson = new Gson();
+            gson.toJson(recordList, bufferedWriter);
+            bufferedWriter.flush();
+
+            fileOutputStream.close();
+
+        } catch (FileNotFoundException e) {
+            recordList = new ArrayList<Record>();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 }
